77cba759f47a6511a1f0342bdb561b20d31f5230
santiagolxx <149891004+SantiagolxxGG@users.noreply.github.com>
2025-04-19 23:22:04 -0400

    Delete src directory

--- src/example/download.js
+++
@@ -1,7 +0,0 @@
-const { downloadMinecraft } = require('../index.js');
-
-downloadMinecraft({
-  root: './minecraft', // RUTA DEL JUEGO
-  version: '1.8', // VERSION A DESCARGAR
-  type: 'release', // TIPO DE VERSION (release - snapshot)
-});

713f506cfdeb670245bed999cc287539de12e704
SantiagolxxGG <username@users.noreply.github.com>
2025-05-13 23:29:55 -0400

    Eliminar archivos innecesarios

--- components/Handler.js
+++
@@ -1,9 +0,0 @@
-/*! Cubic Neutron
- * ©2025 Cubic Neutron - https://github.com/CubicLauncher
-*/
-
-import Downloader from './downloader.js';
-import Launcher from './launcher.js';
-
-
-export { Downloader, Launcher };

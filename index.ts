e30a8f75545ca99a293f7a6e9e6397592cc3edc9
santiagolxx <149891004+SantiagolxxGG@users.noreply.github.com>
2025-05-16 12:21:20 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@
  * ©2025 Cubic Neutron - https://github.com/CubicLauncher
  */
 
-import { getVersions } from "./src/components/others/Manifest";
 import Downloader from "./src/components/downloader/downloader";
 import NeutronLauncher from "./src/components/launcher/launcher";
 

c15693e1ac5c0b4373c35b5f92e8c524109f657a
santiagolxx <149891004+SantiagolxxGG@users.noreply.github.com>
2025-05-16 12:36:07 -0400

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,19 +3,18 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "./index.ts",
+  input: "src/index.ts",
   output: {
     file: "dist/index.js",
     format: "esm",
     sourcemap: true,
   },
   plugins: [
-      typescript({
-      tsconfig: "./tsconfig.json",
-      declaration: false,
+    typescript({
+      tsconfig: "./tsconfig.json"
     }),
-    resolve({ extensions: ['.js', '.ts'] }),
+    resolve({ extensions: [".js", ".ts"] }),
     commonjs()
   ],
-  external: ['child_process', 'events'],
+  external: ["child_process", "events"],
 };

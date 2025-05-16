06061943701a4e939dfee46fd9e959e205b072e2
SantiagolxxGG <username@users.noreply.github.com>
2025-05-15 23:18:47 -0400

    Removed unused files

--- src/components/Handler.ts
+++
@@ -1,9 +0,0 @@
-/*! Cubic Neutron
- * ©2025 Cubic Neutron - https://github.com/CubicLauncher
- */
-
-import { getVersions } from "./others/Manifest";
-import Downloader from "./downloader/downloader";
-import NeutronLauncher from "./launcher/launcher";
-
-export { getVersions, Downloader, NeutronLauncher };
